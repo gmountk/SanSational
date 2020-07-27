781c782074df6692e1cef9b5db9e53b60edb136b
gmountk <gmountk@gmail.com>
2020-07-27 16:57:56 -0500

    Create intraoral.m

---
+++ content/publication/intraoral.m
@@ -0,0 +1,69 @@
++++
+title = "Eating Episode Detection with Jawbone-Mounted Inertial Sensing"
+
+# Date first published.
+date = "2020-01-01"
+
+# Authors. Comma separated list, e.g. `["Bob Smith", "David Jones"]`.
+authors = ["Keum San Chun", "Sarnab Bhattacharya", "Caroline Dolbear", "Jordon Kashanchi","Edison Thomaz"]
+
+# Publication type.
+# Legend:
+# 0 = Uncategorized
+# 1 = Conference proceedings
+# 2 = Journal
+# 3 = Work in progress
+# 4 = Technical report
+# 5 = Book
+# 6 = Book chapter
+publication_types = ["1"]
+
+# Publication name and optional abbreviated version.
+publication = "Proceedings of the 2020 ACM International Symposium on Wearable Computers"
+publication_short = "ISWC"
+
+# Abstract and optional shortened version.
+abstract = "Recent advances in Automated Dietary Monitoring (ADM) with wearables have shown promising results in eating detection in naturalistic environments. However, determining what an individual is consuming remains a significant challenge. In this paper, we present results of a food type classification study based on a sub-centimeter scale wireless intraoral sensor that continuously measures temperature and jawbone movement. We explored the feasibility of classifying nine different types of foods into five classes based on their water-content and typical serving temperature in a controlled environment (n=4). We demonstrated that the system can classify foods into five classes with a weighted accuracy of 77.5%  using temperature-derived features only and with a weighted accuracy of 85.0% using both temperature- and acceleration-derived features. Despite the limitations of our study, these results are encouraging and suggest that intraoral computing might be a viable direction for ADM in the future."
+
+abstract_short = "In this paper, we present results of a food type classification study based on a sub-centimeter scale wireless intraoral sensor that continuously measures temperature and jawbone movement. We explored the feasibility of classifying nine different types of foods into five classes based on their water-content and typical serving temperature in a controlled environment (n=4). We demonstrated that the system can classify foods into five classes with a weighted accuracy of 77.5%  using temperature-derived features only and with a weighted accuracy of 85.0% using both temperature- and acceleration-derived features. Despite the limitations of our study, these results are encouraging and suggest that intraoral computing might be a viable direction for ADM in the future."
+
+# Featured image thumbnail (optional)
+image_preview = ""
+
+# Is this a selected publication? (true/false)
+selected = true
+
+# Projects (optional).
+#   Associate this publication with one or more of your projects.
+#   Simply enter the filename (excluding '.md') of your project file in `content/project/`.
+#   E.g. `projects = ["deep-learning"]` references `content/project/deep-learning.md`.
+projects = [] 
+
+# Links (optional).
+url_pdf = "pdf/chun2020_intraoral_iswc.pdf"
+url_preprint = ""
+url_code = ""
+url_dataset = ""
+url_project = ""
+url_slides = ""
+url_video = ""
+url_poster = ""
+url_source = ""
+
+# Custom links (optional).
+#   Uncomment line below to enable. For multiple links, use the form `[{...}, {...}, {...}]`.
+# url_custom = [{name = "Custom Link", url = "http://example.org"}]
+
+# Does the content use math formatting?
+math = true
+
+# Does the content use source code highlighting?
+highlight = true
+
+# Featured image
+# Place your image in the `static/img/` folder and reference its filename below, e.g. `image = "example.jpg"`.
+[header]
+image = "intraoral_sensor.png"
+caption = ""
+
++++
